3f7763e93b426f8dea6889a2cca5e6c5708021f5
fzabid05 <abidzora88@gmail.com>
2020-11-05 22:11:54 +0100

    add blockquote

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import QuoteCard from "./components/QuoteCard";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Navbar />
+      <QuoteCard />
+      <QuoteCard />
+      <QuoteCard />
+      <QuoteCard />
+      <QuoteCard />
+    </div>
+  );
 }
 
 export default App;
